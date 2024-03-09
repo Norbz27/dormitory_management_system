a5826446fe35187de9ed01bc294d60248fd3038e
Norbz27 <128601526+Norbz27@users.noreply.github.com>
2024-03-09 16:17:53 +0800

    login

--- login.php
+++ login.php
@@ -36,10 +36,10 @@
                     <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
                   </div>
                   <div class="mt-3">
-                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="index.php">SIGN IN</a>
+                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="pages/index.php">SIGN IN</a>
                   </div>
                   <div class="text-center mt-4 font-weight-light">
-                    Don't have an account? <a href="register.html" class="text-primary">Create</a>
+                    Don't have an account? <a href="Signup.php" class="text-primary">Create</a>
                   </div>
                 </form>
               </div>

dbf032f0104b57dd4aa35d0bc1a100a354508077
JezraelSalino <jezraelsalino@yahoo.com>
2024-04-10 14:03:43 +0800

    asdad

--- pages/display_function.inc.php
+++ pages/display_function.inc.php
@@ -2,55 +2,11 @@
 
 include_once '../db/db_conn.php';
 
-function getRoom1() {
+function getRoom1($floor) {
     global $conn;
 
     // Perform the query to fetch paginated rows from the "patients" table
-    $sql = "SELECT * FROM room_details WHERE floor_belong = 1";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        // Fetch all rows into an associative array
-        $patients = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        // Free the result set
-        mysqli_free_result($result);
-
-        // Return the array of patients
-        return $patients;
-    } else {
-        // Handle query error (you may choose to log or display an error message)
-        echo "Error executing query: " . mysqli_error($conn);
-        return array();
-    }
-}
-function getRoom2() {
-    global $conn;
-
-    // Perform the query to fetch paginated rows from the "patients" table
-    $sql = "SELECT * FROM room_details WHERE floor_belong = 2";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        // Fetch all rows into an associative array
-        $patients = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        // Free the result set
-        mysqli_free_result($result);
-
-        // Return the array of patients
-        return $patients;
-    } else {
-        // Handle query error (you may choose to log or display an error message)
-        echo "Error executing query: " . mysqli_error($conn);
-        return array();
-    }
-}
-function getRoom3() {
-    global $conn;
-
-    // Perform the query to fetch paginated rows from the "patients" table
-    $sql = "SELECT * FROM room_details WHERE floor_belong = 3";
+    $sql = "SELECT * FROM room_details WHERE floor_belong = $floor";
     $result = mysqli_query($conn, $sql);
 
     if ($result) {

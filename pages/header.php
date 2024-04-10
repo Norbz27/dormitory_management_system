77660a08cc8f2d633e3f2e6c5461737b28e6f1b4
JezraelSalino <jezraelsalino@yahoo.com>
2024-04-10 23:18:24 +0800

    asdd

--- pages/header.php
+++ pages/header.php
@@ -98,10 +98,10 @@ if (!isset($_SESSION["account"])) {
         <div id="settings-trigger"><i class="ti-settings"></i></div>
         <div id="theme-settings" class="settings-panel">
           <i class="settings-close ti-close"></i>
-          <p class="settings-heading">SIDEBAR SKINS</p>
+          <p class="settings-heading">SURVEY FORM</p>
           <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border mr-3"></div>Light</div>
           <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark</div>
-          <p class="settings-heading mt-2">HEADER SKINS</p>
+          <p class="settings-heading mt-2">SURVEY</p>
           <div class="color-tiles mx-0 px-4">
             <div class="tiles success"></div>
             <div class="tiles warning"></div>
